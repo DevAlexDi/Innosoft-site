e25c6d1f481cdffaa5f2b48a5da2759f5ef0e3ce
keitmark <keitmark@mail.ru>
2019-04-11 10:55:36 +0300

    feat: add projects

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,41 @@ $(document).ready(function(){
             $('.menu').removeClass('menu--black-background');
         }
     });
+
+    //sm modals projects
+
+    $('.modal-open-1').click(function(){
+        $('#modal-yourso').modal('show');
+    });
+    $('.modal-open-2').click(function(){
+        $('#modal-inv').modal('show');
+    });
+    $('.modal-open-3').click(function(){
+        $('#modal-its').modal('show');
+    });
+
+    // sliders init / disabled
+
+    var flagInitSlider = false;
+    $('.modal-project').on('shown.bs.modal', function (e) {
+        $('.modal-slider').slick({
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows:false,
+            dots:true,
+            fade: true
+        });
+        $('.modal-slider').addClass('visible-slider');
+        $('.modal-dialog').addClass('slider-initialized');
+        flagInitSlider = true;
+    });
+    $('.modal-project').on('hidden.bs.modal', function () {
+        if(flagInitSlider){
+            $('.modal-slider').slick('unslick');
+            $('.modal-dialog').removeClass('slider-initialized');
+            $('.modal-slider').removeClass('visible-slider');
+            flagInitSlider = false;
+        }
+    });
 });
